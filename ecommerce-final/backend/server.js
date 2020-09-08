5da0bbff4dfb2c929d992d5a02284dc53306a3ab
SuperMayy <mayowashowumi@outlook.com>
2020-09-08 10:26:54 +0100

    Update

--- ecommerce-final/backend/server.js
+++ ecommerce-final/backend/server.js
@@ -7,6 +7,6 @@ app.get("/api/products", (req, res) => {
     res.send(data.products);
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4040;
 
-app.listen(PORT, () => console.log("API running on port 4000"));
+app.listen(PORT, () => console.log("API running on port 4040"));
